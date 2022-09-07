41f1e7673833c2698efefe9e32682053d4c33379
Sulakshin2001 <nikitasulaaskin@gmail.com>
2022-09-07 19:18:26 +0300

    Updates

---
+++ precache-manifest.21b255bca93fb2b8e5af513a0c46b66b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c06a93a8d57e777acab61bd80d45d1c",
+    "url": "/TodDoList/index.html"
+  },
+  {
+    "revision": "8f2352ee5f7285c1cc7b",
+    "url": "/TodDoList/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/TodDoList/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/TodDoList/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f2352ee5f7285c1cc7b",
+    "url": "/TodDoList/static/js/main.247fd1d4.chunk.js"
+  },
+  {
+    "revision": "260103fedb15b96298bd",
+    "url": "/TodDoList/static/js/runtime-main.eb4371a4.js"
+  }
+]);
